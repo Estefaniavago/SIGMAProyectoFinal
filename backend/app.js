e4b112df3dd556e0f298950aeed843c453abaa51
Estefania Vago <ecvago@hotmail.com>
2026-06-20 09:56:39 -0300

    primer modulo

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 require("./database/connection");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Servidor SIGMA funcionando correctamente");
