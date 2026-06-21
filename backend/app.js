c06f138fceaaa5f2f8391b003a214ca5a5b56305
Estefania Vago <ecvago@hotmail.com>
2026-06-21 20:22:16 -0300

    modulo inspecciones y recorridas

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 require("./database/connection");
 
+
+
+
+const app = express();
+
 const authRoutes = require("./routes/authRoutes");
 const usuarioRoutes = require("./routes/usuarioRoutes");
 const activoRoutes = require("./routes/activoRoutes");
 const ordenTrabajoRoutes = require("./routes/ordenTrabajoRoutes");
-
-const app = express();
+const inspeccionRoutes = require("./routes/inspeccionRoutes");
 
 app.use(express.json());
 
@@ -14,6 +18,7 @@ app.use("/api", authRoutes);
 app.use("/api", usuarioRoutes);
 app.use("/api", activoRoutes);
 app.use("/api", ordenTrabajoRoutes);
+app.use("/api", inspeccionRoutes);
 
 app.get("/", (req, res) => {
     res.send("Servidor SIGMA funcionando correctamente");
